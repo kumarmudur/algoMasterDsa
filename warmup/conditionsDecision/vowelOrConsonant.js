2b00c9f53207dc7062efa2526dbde2adcf488020
kumarmudur <kumarmudur10@gmail.com>
2026-07-10 07:33:26 -0400

    [warmup] vowels and consonants examples

--- warmup/conditionsDecision/vowelOrConsonant.js
+++ warmup/conditionsDecision/vowelOrConsonant.js
@@ -11,3 +11,30 @@ const vowelOrConsonant = (ch) => {
     return "Consonant";
   }
 };
+
+// solution 2
+// time: O(1) | space: O(1)
+const vowelOrConsonant1 = (ch) => {
+  const c = ch.toLowerCase();
+  if (c === "a" || c === "e" || c === "i" || c === "o" || c === "u") {
+    return "Vowel";
+  } else {
+    return "Consonant";
+  }
+};
+
+// solution 3
+// time: O(1) | space: O(1)
+const vowelOrConsonant2 = (ch) => {
+  const c = ch.toLowerCase();
+  switch (c) {
+    case "a":
+    case "e":
+    case "i":
+    case "o":
+    case "u":
+      return "Vowel";
+    default:
+      return "Consonant";
+  }
+};
