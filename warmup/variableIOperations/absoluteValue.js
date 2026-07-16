c3ba3bf8adff51eea4f1470d65308f547cfe1e25
kumarmudur <kumarmudur10@gmail.com>
2026-07-16 09:04:04 -0400

    [basic variables] absoluteValue example

---
+++ warmup/variableIOperations/absoluteValue.js
@@ -0,0 +1,24 @@
+// Absolute Value Without Built-in
+// Example 1:
+// Input: n = -5
+
+// Output: 5
+
+// Example 2:
+// Input: n = 42
+
+// Output: 42
+
+// Constraints:
+// -2^31 <= n <= 2^31 - 1
+// The input fits in a 32-bit signed integer.
+
+// solution 1: Conditional Negation
+/**
+ * @param {number} n
+ * @return {number}
+ */
+// time: O(1) | space: O(1)
+const absoluteValue = (n) => {
+  return n < 0 ? -n : n;
+};
