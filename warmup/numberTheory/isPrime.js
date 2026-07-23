da31637750995dde7fb39389345fb64053a4e4e2
kumarmudur <kumarmudur10@gmail.com>
2026-07-23 08:19:42 -0500

    [basic] prime number check

--- warmup/numberTheory/isPrime.js
+++ warmup/numberTheory/isPrime.js
@@ -31,3 +31,15 @@ const isPrime = (n) => {
   }
   return true;
 };
+
+// solution 2: For Loop
+// time: O(sqrt(n)) | space: O(1)
+const isPrime2 = (n) => {
+  if (n <= 1) return false;
+  if (n === 2) return true;
+  if (n % 2 === 0) return false;
+  for (let i = 3; i * i <= n; i += 2) {
+    if (n % i === 0) return false;
+  }
+  return true;
+};
